f126533bcb17210172b409c8202925e7a4df5510
Krishna-R-Sonar <krishnarsonar@gmail.com>
2025-09-01 20:31:52 +0530

    Configure CORS middleware to allow requests from the specified client URL

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,9 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
